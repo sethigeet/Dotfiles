3b0990bf0200dd64ca78b5aa43195356db7a0d27
Geet Sethi <geetsethi05@gmail.com>
2021-02-16 19:12:33 +0530

    :construction: Changed the window status icons in qtile

--- .config/qtile/bar/widgets/window_name.py
+++ .config/qtile/bar/widgets/window_name.py
@@ -3,8 +3,35 @@ from libqtile import widget
 from defaults.colors import colors
 
 
+class WindowName(widget.WindowName):
+    def update(self, *args):
+        if self.for_current_screen:
+            w = self.qtile.current_screen.group.current_window
+        else:
+            w = self.bar.screen.group.current_window
+        state = ''
+        if w:
+            if w.maximized:
+                state = ' '
+            elif w.minimized:
+                state = ' '
+            elif w.floating:
+                state = ' '
+            var = {}
+            var["state"] = state
+            var["name"] = w.name
+            var["class"] = w.window.get_wm_class()[0] if len(
+                w.window.get_wm_class()) > 0 else ""
+            text = self.format.format(**var)
+            unescaped = self.truncate(text)
+        else:
+            unescaped = self.empty_group_string
+        self.text = unescaped
+        self.bar.draw()
+
+
 def windowName():
-    return widget.WindowName(
+    return WindowName(
         foreground=colors["panel"]["windowName"],
         background=colors["panel"]["bg"],
         padding=0,
