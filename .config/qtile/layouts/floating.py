2d9c3d08bd5f8ebff9ce40d29460436cc9f6b50b
Geet Sethi <geetsethi05@gmail.com>
2021-03-13 18:46:00 +0530

    :construction: Added a rule for the calculator window to be floating

--- .config/qtile/layouts/floating.py
+++ .config/qtile/layouts/floating.py
@@ -5,13 +5,14 @@ from defaults.layout_theme import layout_theme
 
 float_rules = [
     *layout.Floating.default_float_rules,
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
-    Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(wm_class='kite'),  # Kite Co pilot
-    Match(title='branchdialog'),  # gitk
-    Match(title='pinentry'),  # GPG key password entry
+    Match(wm_class="confirmreset"),  # gitk
+    Match(wm_class="makebranch"),  # gitk
+    Match(wm_class="maketag"),  # gitk
+    Match(title="branchdialog"),  # gitk
+    Match(wm_class="ssh-askpass"),  # ssh-askpass
+    Match(wm_class="kite"),  # Kite Co pilot
+    Match(title="pinentry"),  # GPG key password entry
+    Match(title="Calculator"),  # Calculator
 ]
 
 floating_layout = layout.Floating(float_rules=float_rules, **layout_theme)
