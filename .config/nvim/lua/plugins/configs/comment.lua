4cbc0b3a8314c58e4da92814227fa4de6d502cfc
Geet Sethi <geetsethi05@gmail.com>
2021-04-04 15:15:59 +0530

    :recycle: Moved some logic from autogroups to ftplugin in nvim

--- .config/nvim/lua/plugins/configs/comment.lua
+++ .config/nvim/lua/plugins/configs/comment.lua
@@ -2,14 +2,8 @@ local define_augroups = require("general.functions").define_augroups
 
 define_augroups({
     NerdCommenter = {
-        {
-            "FileType", "javascriptreact,typescriptreact",
-            [[setlocal commentstring={/*\ %s\ */}]]
-        },
-        {
-            "BufRead,BufNewFile", "*.{jsx,js}",
-            "setlocal filetype=javascriptreact"
-        },
+        {"FileType", "javascriptreact,typescriptreact", [[setlocal commentstring={/*\ %s\ */}]]},
+        {"BufRead,BufNewFile", "*.{jsx,js}", "setlocal filetype=javascriptreact"},
         {"BufRead,BufNewFile", "*.{tsx}", "setlocal filetype=typescriptreact"}
     }
 })
@@ -35,7 +29,5 @@ function _G.Comment()
     end
 end
 
-vim.api.nvim_set_keymap("v", "<space>/", "v:lua.Comment()",
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<space>/", "v:lua.Comment()",
-                        {noremap = true, silent = true})
+vim.api.nvim_set_keymap("v", "<space>/", "v:lua.Comment()", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<space>/", "v:lua.Comment()", {noremap = true, silent = true})
