8a9c0e8e09f398f360d2e1e1eae7ebe2bccd74e0
Geet Sethi <geetsethi05@gmail.com>
2021-04-07 18:09:39 +0530

    :bug: Removed the color flag from the pgrep alias as pgrep does not support it

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -1,6 +1,5 @@
 # Add color to some shell commands
 alias grep="grep --color=auto"
-alias pgrep="pgrep --color=auto"
 alias egrep="egrep --color=auto"
 alias diff="diff --color=auto"
 alias ip="ip -color=auto"
