c270f36197f1551fcbe247d0fc74c845ab6bee59
Geet Sethi <geetsethi05@gmail.com>
2021-04-09 17:08:38 +0530

    :construction: Removed a keybinding that was interfereing with other keybindings in nvim

--- .config/nvim/lua/plugins/configs/easymotion.lua
+++ .config/nvim/lua/plugins/configs/easymotion.lua
@@ -11,25 +11,17 @@ vim.g.EasyMotion_verbose = 0
 vim.g.EasyMotion_do_shade = 0
 
 -- Repeat last motion
-vim.api
-    .nvim_set_keymap("", "<leader><leader>.", "<Plug>(easymotion-repeat)", {})
+vim.api.nvim_set_keymap("", "<leader><leader>.", "<Plug>(easymotion-repeat)", {})
 
 -- Move to char
-vim.api.nvim_set_keymap("", "<leader><leader>f", "<Plug>(easymotion-overwin-f)",
-                        {})
+vim.api.nvim_set_keymap("", "<leader><leader>f", "<Plug>(easymotion-overwin-f)", {})
 
 -- Move to line
-vim.api.nvim_set_keymap("", "<leader><leader>j",
-                        "<Plug>(easymotion-overwin-line)", {})
-vim.api.nvim_set_keymap("", "<leader><leader>k",
-                        "<Plug>(easymotion-overwin-line)", {})
+vim.api.nvim_set_keymap("", "<leader><leader>j", "<Plug>(easymotion-overwin-line)", {})
+vim.api.nvim_set_keymap("", "<leader><leader>k", "<Plug>(easymotion-overwin-line)", {})
 
 -- s{char}{char} to move to {char}{char}
 vim.api.nvim_set_keymap("n", "s", "<Plug>(easymotion-s2)", {silent = true})
-vim.api.nvim_set_keymap("n", "S", "<Plug>(easymotion-overwin-f2)",
-                        {silent = true})
 
 -- Move to word
-vim.api.nvim_set_keymap("n", "<leader><leader>w",
-                        "<Plug>(easymotion-overwin-w)", {})
-
+vim.api.nvim_set_keymap("n", "<leader><leader>w", "<Plug>(easymotion-overwin-w)", {})
