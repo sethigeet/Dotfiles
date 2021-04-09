49704c821d23777eaab8b31a63bbee3653f4175a
Geet Sethi <geetsethi05@gmail.com>
2021-04-09 17:22:30 +0530

    :construction: Moved some vars to profile from var_exports in shell config

--- .config/shell/var_exports.sh
+++ .config/shell/var_exports.sh
@@ -1,28 +1,5 @@
 export KEYTIMEOUT=10
 
-# Set the encodings
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-# Change the editor to nvim
-if command -v nvim &>/dev/null; then
-	export EDITOR=nvim
-elif command -v vim &>/dev/null; then
-	export EDITOR=vim
-else
-	export EDITOR=vi
-fi
-
-# Preferred editor for local and remote sessions
-# if [ -n $SSH_CONNECTION ]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Change the terminal to alacritty
-export TERMINAL="alacritty"
-
 # export LESS_TERMCAP_md=$'\e[01;31m'
 # export LESS_TERMCAP_me=$'\e[0m'
 # export LESS_TERMCAP_se=$'\e[0m'
@@ -56,29 +33,7 @@ fi
 # export MANPAGER='/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
 # fi
 
-# Ignore some commands from history
-export HISTORY_IGNORE="(l|la|ll|lla|ls|cd|pwd|exit|reboot|history|cd -|cd ..|..|...|....|.....|cs|cau)"
-
 # Anaconda auto completion
 if command -v conda_auto_env &>/dev/null; then
 	export PROMPT_COMMAND="conda_auto_env;$PROMPT_COMMAND"
 fi
-
-# Other XDG paths
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_CONFIG_HOME="$HOME/.config"
-
-# Disable less history
-export LESSHISTFILE="$XDG_DATA_HOME/less/history"
-
-# Fix some paths for programs that do not follow the XDG specs
-export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc-2.0"
-export WGETRC="$XDG_CONFIG_HOME/wget/wgetrc"
-# export ALSA_CONFIG_PATH="$XDG_CONFIG_HOME/alsa/asoundrc"
-# export GNUPGHOME="${XDG_DATA_HOME:-$HOME/.local/share}/gnupg"
-export TMUX_TMPDIR="$XDG_RUNTIME_DIR"
-
-# Set the QT Themes
-export QT_QPA_PLATFORMTHEME="qt5ct"
-# export QT_STYLE_OVERRIDE=kvantum
