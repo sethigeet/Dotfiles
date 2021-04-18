29106a16b532b069552e849f8c7ce7f3e263d4c3
Geet Sethi <geetsethi05@gmail.com>
2021-04-18 10:17:42 +0530

    :construction: Changed some settings for prettier formatting in nvim

--- .config/nvim/lua/plugins/lsp/language-servers/efm/prettier.lua
+++ .config/nvim/lua/plugins/lsp/language-servers/efm/prettier.lua
@@ -1,9 +1,4 @@
 return {
-    formatCommand = (require("plugins.lsp.helpers.get_language_server_path")("prettier", "prettier",
-                                                                             "node") .. [[
-        ${--config-precedence:configPrecedence}
-        ${--tab-width:tabWidth}
-        ${--single-quote:singleQuote}
-        ${--trailing-comma:trailingComma}
-    ]]):gsub("\n", "")
+    formatCommand = require("plugins.lsp.helpers.get_language_server_path")("prettier", "prettier",
+                                                                            "node")
 }
