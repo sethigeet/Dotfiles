0f96f6db186b7cbdca0625648fbc6bf0d76c7a42
Geet Sethi <geetsethi05@gmail.com>
2021-04-21 16:16:15 +0530

    :construction: Added the ability to set fish like abbreviations in zsh

--- .config/zsh/init_zsh.zsh
+++ .config/zsh/init_zsh.zsh
@@ -3,8 +3,8 @@ file="$HOME/.config/shell/init_shell.sh"
 [ -r "$file" ] && [ -f "$file" ] && source "$file"
 
 # Load zsh dotfiles:
-for file in ~/.config/zsh/{p10k.zsh,vi_mode.zsh,var_exports.zsh,keybindings.zsh}; do
-	source "$file"
+for file in ~/.config/zsh/{p10k,vi_mode,var_exports,keybindings,abbr}.zsh; do
+  source "$file"
 done
 
 unset file
