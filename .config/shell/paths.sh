d44eee1b13e3c0a086bc4a46019a06c9b041a2f0
Geet Sethi <geetsethi05@gmail.com>
2021-04-24 13:27:47 +0530

    :construction: Added the cdpath variable to the shell config

--- .config/shell/paths.sh
+++ .config/shell/paths.sh
@@ -1,75 +1,78 @@
 # HOME BIN
 if [ -d "$HOME/.bin" ]; then
-	PATH="$HOME/.bin:$PATH"
+  PATH="$HOME/.bin:$PATH"
 fi
 
 # LOCAL BIN
 if [ -d "$HOME/.local/bin" ]; then
-	PATH="$HOME/.local/bin:$PATH"
+  PATH="$HOME/.local/bin:$PATH"
 fi
 
 # ANDROID STUDIO
 if [ -d "$HOME/Android/Sdk" ]; then
-	export ANDROID_SDK="$HOME/Android/Sdk"
-	export ANDROID_HOME="$HOME/Android/Sdk"
-	PATH="$PATH:$HOME/Android/Sdk/platform-tools"
+  export ANDROID_SDK="$HOME/Android/Sdk"
+  export ANDROID_HOME="$HOME/Android/Sdk"
+  PATH="$PATH:$HOME/Android/Sdk/platform-tools"
 fi
 
 # FLUTTER
 if [ -d "$HOME/snap/flutter" ]; then
-	PATH="$PATH:$HOME/snap/flutter/common/flutter/bin"
+  PATH="$PATH:$HOME/snap/flutter/common/flutter/bin"
 fi
 
 # YARN
 if [ -d "$HOME/.yarn" ]; then
-	PATH="$PATH:$HOME/.yarn/yarn/bin"
+  PATH="$PATH:$HOME/.yarn/yarn/bin"
 fi
 
 # DENO
 if [ -d "$HOME/.deno" ]; then
-	PATH="$PATH:$HOME/.deno/bin"
+  PATH="$PATH:$HOME/.deno/bin"
 fi
 
 # NVM
 export NVM_DIR="$XDG_DATA_HOME/nvm"
 if [ -d "$NVM_DIR" ]; then
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 fi
 
 # GO
 if [ -d "$XDG_DATA_HOME/go" ]; then
-	PATH="$PATH:$XDG_DATA_HOME/go/bin"
-	export GOPATH="$XDG_DATA_HOME/go"
+  PATH="$PATH:$XDG_DATA_HOME/go/bin"
+  export GOPATH="$XDG_DATA_HOME/go"
 fi
 
 # PYENV
 if [ -d "$XDG_DATA_HOME/pyenv" ]; then
-	PATH="$PATH:$XDG_DATA_HOME/pyenv/bin"
-	#eval "$(pyenv init -)"
-	#eval "$(pyenv virtualenv-init -)"
+  PATH="$PATH:$XDG_DATA_HOME/pyenv/bin"
+  #eval "$(pyenv init -)"
+  #eval "$(pyenv virtualenv-init -)"
 fi
 
 # JULIA
 if [ -d "$XDG_DATA_HOME/julia" ]; then
-	PATH="$PATH:$XDG_DATA_HOME/julia/julia-1.5.3/bin"
+  PATH="$PATH:$XDG_DATA_HOME/julia/julia-1.5.3/bin"
 fi
 
 # RUST
 if [ -d "$XDG_DATA_HOME/cargo" ]; then
-	export CARGO_HOME="$XDG_DATA_HOME/cargo"
-	PATH="$PATH:$XDG_DATA_HOME/cargo/bin"
+  export CARGO_HOME="$XDG_DATA_HOME/cargo"
+  PATH="$PATH:$XDG_DATA_HOME/cargo/bin"
 fi
 
 # Doom Emacs
 if [ -d "$HOME/.emacs.d" ]; then
-	PATH="$PATH:$HOME/.emacs.d/bin"
+  PATH="$PATH:$HOME/.emacs.d/bin"
 fi
 
 # Java (OpenJDK)
 if [ -d "/usr/lib/jvm/java-15-openjdk" ]; then
-	export JAVA_HOME="/usr/lib/jvm/java-15-openjdk"
-	PATH="$PATH:/usr/lib/jvm/java-15-openjdk/bin"
+  export JAVA_HOME="/usr/lib/jvm/java-15-openjdk"
+  PATH="$PATH:/usr/lib/jvm/java-15-openjdk/bin"
 fi
 
 export PATH
+
+# CDPATH
+export CDPATH="$CDPATH:$HOME/Repos:$XDG_CONFIG_HOME"
