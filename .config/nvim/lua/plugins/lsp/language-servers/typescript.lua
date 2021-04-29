ab759f67f08044e29a86aa46620625c70b6236d4
Geet Sethi <geetsethi05@gmail.com>
2021-04-29 16:56:00 +0530

    :bug: Disabled formatting by tsserver as prettier was formatting the files in nvim

--- .config/nvim/lua/plugins/lsp/language-servers/typescript.lua
+++ .config/nvim/lua/plugins/lsp/language-servers/typescript.lua
@@ -1,6 +1,8 @@
 require("lspconfig").typescript.setup({
     root_dir = require("plugins.lsp.helpers.root_dir"),
     on_attach = function(client, bufnr)
+        -- Disable formatting in the lsp server as prettier formats the code
+        client.resolved_capabilities.document_formatting = false
         require("plugins.lsp.helpers.document_highlight")(client)
         require("plugins.lsp.helpers.auto_format")("typescript", client)
     end,
@@ -9,4 +11,3 @@ require("lspconfig").typescript.setup({
             "typescript")
     }
 })
-
