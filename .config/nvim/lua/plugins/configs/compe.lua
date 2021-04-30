4fb7282b9ba2e87cdeef1105fef0f915d1369e5c
Geet Sethi <geetsethi05@gmail.com>
2021-04-30 13:02:36 +0530

    :construction: Improved the completion in nvim

--- .config/nvim/lua/plugins/configs/compe.lua
+++ .config/nvim/lua/plugins/configs/compe.lua
@@ -33,6 +33,7 @@ require("compe").setup({
 
 vim.g["compe.source.vim_dadbod_completion"] = true
 
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
--- .config/nvim/lua/plugins/lsp/language-servers/go.lua
+++ .config/nvim/lua/plugins/lsp/language-servers/go.lua
@@ -1,6 +1,7 @@
 require("lspconfig").go.setup({
     settings = { gopls = { analyses = { unusedparams = true }, staticcheck = true } },
     root_dir = require("plugins.lsp.helpers.root_dir"),
+    init_options = { usePlaceholders = true },
     on_attach = function(client, bufnr)
         require("plugins.lsp.helpers.document_highlight")(client)
         require("plugins.lsp.helpers.auto_format")("go", client)
