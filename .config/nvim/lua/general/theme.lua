0f350aed5f832b26aba71b996e72b7be94aa7778
Geet Sethi <geetsethi05@gmail.com>
2021-05-07 16:25:10 +0530

    :lipstick: Changed the colors being shown in git diffs in nvim

--- .config/nvim/lua/general/theme.lua
+++ .config/nvim/lua/general/theme.lua
@@ -4,3 +4,8 @@ vim.cmd("colorscheme " .. Opts.colorscheme)
 
 -- Syntax highlighting
 vim.g.python_highlight_all = 1
+
+-- Git diff colors
+vim.cmd("highlight DiffAdd guifg=NONE guibg=#4D5E49")
+vim.cmd("highlight DiffChange guifg=NONE guibg=#49545e")
+vim.cmd("highlight DiffDelete guifg=NONE guibg=#5C4945")
