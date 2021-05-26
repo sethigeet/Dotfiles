eac6139399067589300cb4ab45d0ffcd50f11a54
Geet Sethi <geetsethi05@gmail.com>
2021-05-26 15:18:59 +0530

    :recycle: Simplified the web-search function in the shell config and formatted the function.sh file

--- .config/shell/functions.sh
+++ .config/shell/functions.sh
@@ -1,41 +1,30 @@
 vicd() {
-	local dest
-	dest="$(command vifm --choose-dir - "$@")"
-	if [ -z "$dest" ]; then
-		echo 'Directory picking cancelled/failed'
-		return 1
-	fi
-	cd "$dest" || (echo "Invalid directory name!" && return 1)
+  local dest
+  dest="$(command vifm --choose-dir - "$@")"
+  if [ -z "$dest" ]; then
+    echo 'Directory picking cancelled/failed'
+    return 1
+  fi
+  cd "$dest" || (echo "Invalid directory name!" && return 1)
 }
 
 function web_search() {
-	google="https://www.google.com/search?q="
-	ddg="https://www.duckduckgo.com/?q="
-	ddgl="https://lite.duckduckgo.com/lite/?kd=-1&kp=-1&q="
+  google="https://www.google.com/search?q="
+  ddg="https://www.duckduckgo.com/?q="
+  ddgl="https://lite.duckduckgo.com/lite/?kd=-1&kp=-1&q="
 
-	if [ "$1" = "google" ]; then
-		engine="$google"
-	elif [ "$1" = "ddg" ]; then
-		engine="$ddg"
-	elif [ "$1" = "ddgl" ]; then
-		engine="$ddgl"
-	fi
-
-  # search or go to main page depending on number of arguments passed
-  if [[ $# -gt 1 ]]; then
-    # build search url:
-    # join arguments passed with '+', then append to search engine URL
-    url="$engine${(j:+:)@[2,-1]}"
-  else
-    # build main page url:
-    # split by '/', then rejoin protocol (1) and domain (2) parts with '//'
-    url="${(j://:)${(s:/:)urls[$1]}[1,2]}"
+  if [ "$1" = "google" ]; then
+    engine="$google"
+  elif [ "$1" = "ddg" ]; then
+    engine="$ddg"
+  elif [ "$1" = "ddgl" ]; then
+    engine="$ddgl"
   fi
 
-  lynx "$url"
+  shift
+  lynx "$engine$*"
 }
 
 ec() {
-	$EDITOR "$(which "$1")"
+  $EDITOR "$(which "$1")"
 }
-
