d09a2a0960878052509d15245734bb1fe3c12970
Geet Sethi <geetsethi05@gmail.com>
2021-06-18 14:51:21 +0530

    :construction: Updated the mouse bindings in qtile for easily resizing floating windows

--- .config/qtile/bindings/mouse.py
+++ .config/qtile/bindings/mouse.py
@@ -8,5 +8,7 @@ mouse = [
          start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
+    Drag([mod, "shift"], "Button1", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front())
 ]
