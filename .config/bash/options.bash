2dabe2a42967499e474790d514c4db05040f3366
Geet Sethi <geetsethi05@gmail.com>
2021-06-26 19:32:22 +0530

    :construction: Added some options in the bash config

--- .config/bash/options.bash
+++ .config/bash/options.bash
@@ -1,6 +1,8 @@
 # Set Options
-shopt -s autocd
-shopt -s histappend
+shopt -s autocd                   # cd without typing `cd`
+shopt -s histappend               # append to the history file rather than reqriting it
+shopt -s dirspell                 # automatically try to correct directory names when providing tab completion
+shopt -s globstar dotglob extglob # globbing options
 
 # Use bash-completion, if available
 [[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] \
