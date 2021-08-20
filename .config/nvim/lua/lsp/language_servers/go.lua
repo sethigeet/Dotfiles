c3324626db7201f663fb9fc1e3b8cf033ae6dc86
Geet Sethi <geetsethi05@gmail.com>
2021-08-20 17:54:59 +0530

    :bug: Fixed some settings for gopls in nvim config

--- .config/nvim/lua/lsp/language_servers/go.lua
+++ .config/nvim/lua/lsp/language_servers/go.lua
@@ -3,22 +3,27 @@ local LanguageServer = require("lsp.helpers.create_language_server")
 local ls = LanguageServer:create({
   lang = "go",
   custom = {
-    settings = { gopls = { analyses = { unusedparams = true }, staticcheck = true } },
-    init_options = { usePlaceholders = true },
-    gopls = {
-      analyses = { unusedparams = true, unreachable = false },
-      codelenses = {
-        generate = true, -- show the `go generate` lens.
-        gc_details = true, -- Show a code lens toggling the display of gc's choices.
+    settings = {
+      gopls = {
+        analyses = { unusedparams = true },
+        staticcheck = true,
+        codelenses = {
+          generate = true, -- show the `go generate` lens.
+          gc_details = true, -- Show a code lens toggling the display of gc's choices.
+        },
+        usePlaceholders = true,
+        completeUnimported = true,
+        matcher = "fuzzy",
+        symbolMatcher = "fuzzy",
+        gofumpt = false, -- turn on for new repos, gofumpt is good but also create code turmoils
       },
-      usePlaceholders = true,
-      completeUnimported = true,
-      staticcheck = true,
-      matcher = "fuzzy",
-      symbolMatcher = "fuzzy",
-      gofumpt = false, -- turn on for new repos, gofumpt is good but also create code turmoils
     },
+    init_options = { usePlaceholders = true },
   },
+  on_attach = function(client, _)
+    -- Disable formatting in the lsp server as `vim-go` plugin handles formatting
+    client.resolved_capabilities.document_formatting = false
+  end,
 })
 
 return ls
