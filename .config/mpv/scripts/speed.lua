e6e6a13f7a850c03c81bccce1e86fbfd249078d9
Geet Sethi <geetsethi05@gmail.com>
2022-01-07 14:27:23 +0530

    :arrow_up: Updated the mpv config and scripts to work with new api changes in mpv

--- .config/mpv/scripts/speed.lua
+++ .config/mpv/scripts/speed.lua
@@ -2,15 +2,15 @@ local running = false
 local timer
 
 function show_current_speed()
-    running = not running
-    if running then
-        timer = mp.add_periodic_timer(1, function()
-            local currentspeed = mp.get_property("speed")
-            mp.osd_message("Speed: " .. ("%.2f"):format(math.floor(currentspeed * 1000) / 1000), 1)
-        end)
-    else
-        timer:stop()
-    end
+	running = not running
+	if running then
+		timer = mp.add_periodic_timer(1, function()
+			local currentspeed = mp.get_property("speed")
+			mp.osd_message("Speed: " .. ("%.2f"):format(math.floor(currentspeed * 1000) / 1000), 1)
+		end)
+	else
+		timer:stop()
+	end
 end
 
 mp.add_key_binding(nil, "show-current-speed", show_current_speed)
