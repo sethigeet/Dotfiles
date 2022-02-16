1294782bda756765347cf7381086129eba65ace7
Geet Sethi <geetsethi05@gmail.com>
2022-02-16 14:08:48 +0530

    :bug: Fix colorscheme in nvim

--- .config/nvim/lua/plugins/configs/tokyonight.lua
+++ .config/nvim/lua/plugins/configs/tokyonight.lua
@@ -3,7 +3,6 @@ local Plugin = require("plugins.plugin")
 return Plugin:create({
   configure = function()
     vim.g.tokyonight_style = "night"
-    require("tokyonight").colorscheme()
-    -- vim.cmd("colorscheme tokyonight")
+    vim.cmd("colorscheme tokyonight")
   end,
 })
