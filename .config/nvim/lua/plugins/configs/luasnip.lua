fb210d2e9f7b7ceecdfa33dca6d8d132e626d6e9
Geet Sethi <geetsethi05@gmail.com>
2022-03-15 20:46:54 +0530

    :construction: Start using actual powers of luasnip in nvim

--- .config/nvim/lua/plugins/configs/luasnip.lua
+++ .config/nvim/lua/plugins/configs/luasnip.lua
@@ -1,5 +1,26 @@
 local Plugin = require("plugins.plugin")
 
+local function make_snips(snips, ls)
+  local result = {}
+  for trig, exp in pairs(snips) do
+    local expansion
+    if type(exp) == "string" then
+      expansion = { ls.text_node({ exp }), ls.insert_node(0) }
+    elseif type(exp) == "table" then
+      for k, v in ipairs(exp) do
+        if type(v) == "string" then
+          exp[k] = ls.text_node({ v })
+        end
+      end
+      expansion = exp
+    end
+
+    table.insert(result, ls.s({ trig = trig, desc = exp.desc }, expansion))
+  end
+
+  return result
+end
+
 return Plugin:create({
   configure = function()
     local luasnip = require("luasnip")
@@ -9,7 +30,29 @@ return Plugin:create({
       -- Update more often, :h events for more info.
       updateevents = "TextChanged,TextChangedI",
     })
-    -- Load snippets
+
+    -- Load snippets from plugins
     require("luasnip/loaders/from_vscode").lazy_load()
+
+    -- Load custom snippets
+    for _, file in ipairs(vim.api.nvim_get_runtime_file("lua/snippets/*.lua", true)) do
+      local name = vim.fn.fnamemodify(file, ":t:r")
+      local contents = require("snippets." .. name)
+
+      local fts, snips = contents["filetypes"], contents["snippets"]
+      if type(fts) == "table" then
+        for _, ft in ipairs(fts) do
+          luasnip.snippets[ft] = luasnip.snippets[ft] or {}
+          for _, snip in ipairs(make_snips(snips, luasnip)) do
+            table.insert(luasnip.snippets[ft], snip)
+          end
+        end
+      elseif type(fts) == "string" then
+        luasnip.snippets[fts] = luasnip.snippets[fts] or {}
+        for _, snip in ipairs(make_snips(snips, luasnip)) do
+          table.insert(luasnip.snippets[fts], snip)
+        end
+      end
+    end
   end,
 })
---
+++ .config/nvim/lua/snippets/lua.lua
@@ -0,0 +1,33 @@
+local ls = require("luasnip")
+local sn = ls.sn
+local i = ls.insert_node
+local t = ls.text_node
+local d = ls.dynamic_node
+local c = ls.choice_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+local require_var = function(args, _)
+  local text = args[1][1] or ""
+  local split = vim.split(text, ".", { plain = true })
+
+  local options = {}
+  for len = 0, #split - 1 do
+    table.insert(options, t(table.concat(vim.list_slice(split, #split - len, #split), "_")))
+  end
+
+  return sn(nil, {
+    c(1, options),
+  })
+end
+
+return {
+  filetypes = "lua",
+  snippets = {
+    ignore = "--stylua: ignore",
+
+    req = fmt([[local {} = require("{}")]], {
+      d(2, require_var, { 1 }),
+      i(1),
+    }),
+  },
+}
