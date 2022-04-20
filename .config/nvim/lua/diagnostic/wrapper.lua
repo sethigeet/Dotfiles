a11cfadfa2af0289c79269dde834bcd54d473812
Geet Sethi <geetsethi05@gmail.com>
2022-04-20 13:49:20 +0530

    :alien: Updated the nvim config to work with the new breaking changes

--- .config/nvim/lua/diagnostic/wrapper.lua
+++ .config/nvim/lua/diagnostic/wrapper.lua
@@ -1,30 +1,30 @@
 -- Get diagnostic
-vim.api.nvim_add_user_command("DiagnosticGetAll", function()
+vim.api.nvim_create_user_command("DiagnosticGetAll", function()
   vim.diagnostic.get()
 end, { desc = "Get all the available diagnostics" })
-vim.api.nvim_add_user_command("DiagnosticGetNext", function()
+vim.api.nvim_create_user_command("DiagnosticGetNext", function()
   vim.diagnostic.get_next()
 end, { desc = "Get the next diagnostic" })
-vim.api.nvim_add_user_command("DiagnosticGetPrev", function()
+vim.api.nvim_create_user_command("DiagnosticGetPrev", function()
   vim.diagnostic.get_prev()
 end, { desc = "Get the previous diagnostic" })
 
 -- Goto diagnostic
-vim.api.nvim_add_user_command("DiagnosticGotoNext", function()
+vim.api.nvim_create_user_command("DiagnosticGotoNext", function()
   vim.diagnostic.goto_next()
 end, { desc = "Go to the next diagnostic" })
-vim.api.nvim_add_user_command("DiagnosticGotoPrev", function()
+vim.api.nvim_create_user_command("DiagnosticGotoPrev", function()
   vim.diagnostic.goto_prev()
 end, { desc = "Go to the previous diagnostic" })
 
 -- Location list
-vim.api.nvim_add_user_command("DiagnosticPopulateLocList", function()
+vim.api.nvim_create_user_command("DiagnosticPopulateLocList", function()
   vim.diagnostic.set_loclist({ open_loclist = false })
 end, { desc = "Populate the location list with the available diagnostics" })
 
 -- Virtual text
 local show_virtual_text = true
-vim.api.nvim_add_user_command("DiagnosticToggleVirtualText", function()
+vim.api.nvim_create_user_command("DiagnosticToggleVirtualText", function()
   show_virtual_text = not show_virtual_text
 
   vim.diagnostic.config({
