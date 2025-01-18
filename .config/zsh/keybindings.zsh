66dc75fc91dae0cbd58b97a1cc32b0cfa13d3611
Geet Sethi <geetsethi05@gmail.com>
2025-01-18 10:45:56 +0530

    :construction: add better settings for history management in zsh

--- .config/zsh/keybindings.zsh
+++ .config/zsh/keybindings.zsh
@@ -2,3 +2,7 @@ bindkey -s '^o' 'vicd\n'
 
 # Use Ctrl+Backspace to delete a word
 bindkey '^\b' backward-delete-word
+
+# completion using arrow keys (based on history)
+bindkey '^[[A' history-search-backward
+bindkey '^[[B' history-search-forward
--- .config/zsh/var_exports.zsh
+++ .config/zsh/var_exports.zsh
@@ -1,3 +1,8 @@
 HISTSIZE=100000
 SAVEHIST=100000
 HISTFILE="$XDG_DATA_HOME/zsh/history"
+
+setopt share_history 
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_verify
